7fd890054b3971bc25832887b14bcf6c9cbcba25
hkimca <harrison.kim.ca@gmail.com>
2017-07-10 11:22:05 -0400

    Initial Commit

---
+++ w3d1_myOwnScrollViewTests/w3d1_myOwnScrollViewTests.m
@@ -0,0 +1,39 @@
+//
+//  w3d1_myOwnScrollViewTests.m
+//  w3d1_myOwnScrollViewTests
+//
+//  Created by Seantastic31 on 10/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface w3d1_myOwnScrollViewTests : XCTestCase
+
+@end
+
+@implementation w3d1_myOwnScrollViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
